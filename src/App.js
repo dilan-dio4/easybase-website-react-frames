4b4c03789c6f67157d77f963dd56be76cbe31141
Dilan <dio4@pitt.edu>
2021-03-21 22:09:41 -0400

    f

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import './bootstrap.css';
 import Functions from './Functions';
 import theme from './theme';
@@ -7,11 +6,13 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-      <CssBaseline />
-      <Functions />
-    </ThemeProvider>
+    <div style={{ height: 850 }}>
+      <ThemeProvider theme={theme}>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <CssBaseline />
+        <Functions />
+      </ThemeProvider>
+    </div>
   );
 }
 
